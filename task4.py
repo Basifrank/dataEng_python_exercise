424ddebf8d9104a6359cc3585a044a432cbb9a69
basifrank <obasichigoziefrancis@gmail.com>
2025-03-13 20:24:07 +0300

    Updated task4

--- task4.py
+++ task4.py
@@ -5,7 +5,7 @@ def checkNames(names):
     if len(notalpha) > 0:
         raise TypeError(f"These are the invalid names: {notalpha}")
     else:
-        print("All names are valid")
+        return print("All names are valid")
         
         
 checkNames(customer_names)
